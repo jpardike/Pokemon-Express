d66b24ad7673e5ea5e4797ca0345fa69acd56b10
jpardike2 <jpardike@gmail.com>
2020-10-01 15:24:00 -0700

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 
+// config
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+// Pokemon model
+const pokemon = require('./pokemon');
+
+app.get('/pokemon', (req, res) => {
+  res.send(pokemon);
+})
 
 app.listen(PORT, () => console.log(`Server started successfully on port ${PORT}`));
